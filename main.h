87f7325f82f75e5e44718bb3752a779599be99c3
uelkariuki <cratzuel@gmail.com>
2025-02-17 17:18:29 +0300

    update main.h file with new prototypes

--- main.h
+++ main.h
@@ -16,6 +16,8 @@ int print_int(va_list args);
 int print_str(va_list args);
 int print_binary(va_list args);
 int print_char(va_list args);
+int print_unsigned(va_list args);
+int print_octal(va_list args);
 
 
 specifier_t *get_specifiers(void);
@@ -26,6 +28,7 @@ void _itoa(int num, char s[]);
 int _strlen(char *s);
 void to_binary(unsigned int k, char *s);
 void _uitoa(unsigned int num, char s[]);
+void to_octal(unsigned int k, char *s);
 
 #endif
 
