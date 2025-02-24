a8f58e1c0925e10513c88fc2e9b00201788b0060
uelkariuki <cratzuel@gmail.com>
2025-02-24 22:30:13 +0300

    Add function that deals with non printable characters

--- more_specifier_functions.c
+++ more_specifier_functions.c
@@ -81,5 +81,38 @@ int print_hex(va_list args, buffer_t *buf) {
 		}
 	}
 	return i;
-	
+
+}
+
+/**
+ * print_String - prints converted string to the standard output
+ * @args: The argument list containing the string to be converted
+ *
+ * Return: The number of characters printed on success, -1 on failure
+*/
+int print_String(va_list args, buffer_t *buf) {
+	int step, i = 0;
+	char *str;
+
+	str = va_arg(args, char *);
+	if (!str) str = "(null)";
+	while (str[i]) {
+		if ((str[i] > 0 && str[i] < 32) || str[i] >= 127) {
+			step = str_to_ASCII(&str[i], buf);
+			i += step;
+		} else {
+			buf->buffer[buf->index++] = str[i];
+			buf->count++;
+
+			if (buf->index == 1024) {
+				write(1, buf->buffer, buf->index);
+				buf->index = 0;
+			}
+			i++;
+		}
+
+
+	}
+	return i;
+
 }
